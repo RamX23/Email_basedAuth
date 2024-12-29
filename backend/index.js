52df2dd1e7827d01deec9f2698c29ea259ec8fc7
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-29 15:04:49 +0530

    Update index.js "nodeenv commit"

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,16 @@ app.use("/api/auth", authRoutes);
 app.use('/api/auth', fileRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
+app.get('/',(req,res)=>{
+	res.send("Hello! from serve")
+})
 
 app.listen(PORT, () => {
 	console.log("Server is running on port: ", process.env.PORT); 
